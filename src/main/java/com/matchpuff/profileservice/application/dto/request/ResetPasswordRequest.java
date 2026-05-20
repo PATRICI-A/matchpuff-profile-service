1afbba3bfa53aaf5b5af683285101c02675d5399
Andrew7480 <andrewc7480@gmail.com>
2026-05-20 17:22:48 -0500

    adding new reset password endpoint

---
+++ src/main/java/com/matchpuff/profileservice/application/dto/request/ResetPasswordRequest.java
@@ -0,0 +1,16 @@
+package com.matchpuff.profileservice.application.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+
+@Data
+public class ResetPasswordRequest {
+
+    @NotBlank(message = "New password must not be blank")
+    @Pattern(
+        regexp = "^(?=.*[A-Z])(?=.*[!@#$,.]).{8,100}$",
+        message = "Password must be at least 8 characters, contain at least one uppercase letter and one of: !@#$,."
+    )
+    private String newPassword;
+}
